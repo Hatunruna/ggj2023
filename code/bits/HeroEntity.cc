b8b4df487e54e8cedd1f5ce3f684eb59b83a7beb
ahugeat <hugeat.arthur@gmail.com>
2023-01-19 22:02:42 +0100

    Render other hero when he is in field of vision

--- code/bits/HeroEntity.cc
+++ code/bits/HeroEntity.cc
@@ -18,20 +18,45 @@ namespace xy {
 
   void HeroEntity::render(gf::RenderTarget &target, const gf::RenderStates &states) {
     gf::CircleShape hero(16.0f);
+    gf::Color4f currentColor;
+    gf::Color4f otherColor;
+    const HeroState* currentState = nullptr;
+    const HeroState* otherState = nullptr;
+    const gf::SquareMap* currentFov = nullptr;
 
     switch (m_hero) {
       case Hero::Lisa:
-        hero.setColor(gf::Color::Green);
-        hero.setPosition(m_state.lisa.position * CellSize + CellSize / 2);
+        currentColor = gf::Color::Green;
+        otherColor = gf::Color::Blue;
+        currentState = &m_state.lisa;
+        otherState = &m_state.ryan;
+        currentFov = &m_state.lisaMap.levelsFov[currentState->levelIndex];
         break;
       case Hero::Ryan:
-        hero.setColor(gf::Color::Blue);
-        hero.setPosition(m_state.ryan.position * CellSize + CellSize / 2);
+        currentColor = gf::Color::Blue;
+        otherColor = gf::Color::Green;
+        currentState = &m_state.ryan;
+        otherState = &m_state.lisa;
+        currentFov = &m_state.ryanMap.levelsFov[currentState->levelIndex];
         break;
     }
 
-    hero.setAnchor(gf::Anchor::Center);
-    target.draw(hero, states);
+    assert(currentState != nullptr && otherState != nullptr && currentFov != nullptr);
+
+    auto renderHero = [&hero, &target, &states](const HeroState* heroState, const gf::Color4f& color) {
+      hero.setColor(color);
+      hero.setPosition(heroState->position * CellSize + CellSize / 2);
+      hero.setAnchor(gf::Anchor::Center);
+      target.draw(hero, states);
+    };
+
+    // Render current
+    renderHero(currentState, currentColor);
+
+    // Render other only on the same level
+    if (currentState->levelIndex == otherState->levelIndex && currentFov->isInFieldOfVision(otherState->position)) {
+      renderHero(otherState, otherColor);
+    }
   }
 
 }
