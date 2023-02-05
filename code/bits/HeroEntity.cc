7c824f929743b46cff9d6a04b10fad799f573691
ahugeat <hugeat.arthur@gmail.com>
2023-02-05 03:42:13 +0100

    Cleanup code

--- code/bits/HeroEntity.cc
+++ code/bits/HeroEntity.cc
@@ -58,9 +58,6 @@ namespace rc {
   }
 
   void HeroEntity::render(gf::RenderTarget &target, const gf::RenderStates &states) {
-    gf::CircleShape hero(16.0f);
-    gf::Color4f localColor;
-    gf::Color4f otherColor;
     const HeroState& localState = m_state.localPlayer(m_hero).hero;
     const HeroState& otherState = m_state.otherPlayer(m_hero).hero;
     const gf::SquareMap& localLevel = m_state.localPlayer(m_hero).map.levels[localState.levelIndex].map;
@@ -69,33 +66,6 @@ namespace rc {
     if (localState.levelIndex == otherState.levelIndex && localLevel.isInFieldOfVision(otherState.position)) {
       m_otherEntity->renderAnimation(target, states);
     }
-
-/*
-    switch (m_hero) {
-      case Hero::Lisa:
-        localColor = gf::Color::Green;
-        otherColor = gf::Color::Blue;
-        break;
-      case Hero::Ryan:
-        localColor = gf::Color::Blue;
-        otherColor = gf::Color::Green;
-        break;
-    }
-
-    auto renderHero = [&hero, &target, &states](const HeroState& heroState, const gf::Color4f& color) {
-      hero.setColor(color);
-      hero.setPosition(heroState.middle + CellSize / 2);
-      hero.setAnchor(gf::Anchor::Center);
-      target.draw(hero, states);
-    };
-
-    // Render current
-    renderHero(localState, localColor);
-
-    // Render other only on the same level
-    if (localState.levelIndex == otherState.levelIndex && localLevel.isInFieldOfVision(otherState.position)) {
-      renderHero(otherState, otherColor);
-    }*/
   }
 
   void HeroEntity::renderAnimation(gf::RenderTarget &target, const gf::RenderStates &states) {
